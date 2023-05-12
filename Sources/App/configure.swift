45b4846ad35669841430cf5e5dc4f44b58e7c667
0xifarouk <33427443+FaroukZeino@users.noreply.github.com>
2023-05-12 23:21:33 +0300

    fix depreciation notice for database configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,13 +7,13 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
+    app.databases.use(.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tls: .disable)), as: .psql)
 
     app.migrations.add(CreateTodo())
 
